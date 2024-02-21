6151ffeed9f476a11a64514422fbe5b89f407679
SripaliManimekala <sripalimanimekala@gmail.com>
2024-02-22 02:39:53 +0530

    created customer model

--- ems-backend/src/main/java/com/springproject/emsbackend/model/Customer.java
+++ ems-backend/src/main/java/com/springproject/emsbackend/model/Customer.java
@@ -1,4 +1,29 @@
 package com.springproject.emsbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+@Generated
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name="date_of_birth", nullable = false)
+    private Date dateOfBirth;
+
+    @Column(name="nic_number", unique = true, nullable = false)
+    private String nicNumber;
 }
