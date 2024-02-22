19ff6cf080c6c24e8593749b89f576b7e56150f1
SripaliManimekala <sripalimanimekala@gmail.com>
2024-02-22 09:46:00 +0530

    created Fammember model

--- ems-backend/src/main/java/com/springproject/emsbackend/model/FamilyMember.java
+++ ems-backend/src/main/java/com/springproject/emsbackend/model/FamilyMember.java
@@ -1,4 +1,27 @@
 package com.springproject.emsbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="family_member")
 public class FamilyMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "primary_customer_id", nullable = false)
+    private Customer primaryCustomer;
+
+    @ManyToOne
+    @JoinColumn(name = "family_member_id", nullable = false)
+    private Customer familyMember;
 }
