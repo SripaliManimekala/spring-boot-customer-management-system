c7b2c5c90285e9d4aef3c58c11e8f2785dc2dc74
SripaliManimekala <sripalimanimekala@gmail.com>
2024-02-22 09:51:05 +0530

    created NIC model

--- ems-backend/src/main/java/com/springproject/emsbackend/model/NICImage.java
+++ ems-backend/src/main/java/com/springproject/emsbackend/model/NICImage.java
@@ -1,4 +1,27 @@
 package com.springproject.emsbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="nic_image")
 public class NICImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="customer_id", nullable = false)
+    private Customer customer;
+
+    @Lob
+    private byte[] image;
 }
